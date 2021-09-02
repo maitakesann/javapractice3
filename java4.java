11e85e54808172a475046ed628c519fc2a6b6f96
maitakesann <kankore1204a@yahoo.co.jp>
2021-09-02 07:01:19 +0000

    メソッド　引数

--- java4.java
+++ java4.java
@@ -1,11 +1,11 @@
 public class java4{
-    public static void main(String[] args) {
-        System.out.println("メソッドを引き出します");
-        hello("kk");
-        hello("jj");
-        hello("ii");
+    public static void main(String[] args)  {
+        add(100,20);
+        add(200,50);
     }
-    public static void hello(String name){
-        System.out.println(name + "さんです");
+    public static void add(int x, int y){
+        int ans = x + y;
+        System.out.println(x + "+" + y + "=" + ans);
     }
+
 }
