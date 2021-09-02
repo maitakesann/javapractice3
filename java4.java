ece00f28159f79b8a152ba50668df5340233c04e
maitakesann <kankore1204a@yahoo.co.jp>
2021-09-02 06:15:20 +0000

    配列ループぶん

--- java4.java
+++ java4.java
@@ -1,5 +1,12 @@
 public class java4{
     public static void main(String[] args) {
-        System.out.println("Helow");  
+        int[] numbers = {3,4,9};
+        System.out.println("1桁の数字を入力してください");
+        int input = new java.util.Scanner(System.in).nextInt();
+        for (int number:numbers){
+            if(number == input){
+                System.out.println("アタリ！");
+            }
+        }
     }
 }
