b798db6239ddde9cb0929c90950cac8a2680bc44
maitakesann <kankore1204a@yahoo.co.jp>
2021-09-05 08:49:13 +0000

    引数

--- java4.java
+++ java4.java
@@ -1,11 +1,10 @@
 public class java4{
     public static void main(String[] args)  {
-       int ans = add(100,100);
-        System.out.println(ans);
-    
+        add(50,60);
     }
-    public static int add(int x, int y){
+    public static void add( int x , int y){
         int ans = x + y;
-        return ans;
+        System.out.println(x +"+"+ y + "=" + ans);
     }
+
 }
