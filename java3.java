3342f51aa0f112d5064bbebd79dbe5b081206fab
maitakesann <kankore1204a@yahoo.co.jp>
2021-09-05 08:30:06 +0000

    数当てゲーム復習

--- java3.java
+++ java3.java
@@ -13,5 +13,18 @@ public class java3{
         if (isHungry == 1){
             System.out.println(food +"をいただきます");
         }
+        
+        System.out.println("数当てゲーム");
+        int ans = new java.util.Random().nextInt(10);
+        for (int i = 0; i < 5 ; i++){
+            System.out.println("0~9の数字を入れてください");
+            int num = new java.util.Scanner(System.in).nextInt();
+            if(ans == num){
+                System.out.println("アタリ");
+            }else{
+                System.out.println("違います");
+            }
+        }
+        System.out.println("ゲームを終了します");
     }
 }
