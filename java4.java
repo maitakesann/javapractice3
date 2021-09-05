d341712b6a08f34fbeb52cb3de3e797699243482
maitakesann <kankore1204a@yahoo.co.jp>
2021-09-05 09:44:01 +0000

    引数戻り値

--- java4.java
+++ java4.java
@@ -1,10 +1,17 @@
 public class java4{
-    public static void main(String[] args)  {
-        add(50,60);
+    public static int add(int x, int y)  {
+      return x + y;
     }
-    public static void add( int x , int y){
-        int ans = x + y;
-        System.out.println(x +"+"+ y + "=" + ans);
+    public static double add(double x , double y){
+        return x + y;
     }
-
+    public static String add(String x , String y){
+        return x + y;
+    }
+    
+    public static void main (String[] args){
+        System.out.println(add(5,6));
+        System.out.println(add(4.8,6.8));
+        System.out.println(add("じゃじゃ","かか"));
+    }
 }
