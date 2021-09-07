f96c8054230200d867d623be0acbb36de4dd2ffe
maitakesann <kankore1204a@yahoo.co.jp>
2021-09-07 08:39:23 +0000

    サイコロランダム

--- java3.java
+++ java3.java
@@ -1,30 +1,7 @@
 public class java3{
     public static void main(String[] args){
-        int isHungry = 1; String food = "Egg";
+        int dice = (int)(Math.random()*7) + 1; //1~7までの乱数を発生させる
+        System.out.println("サイコろの目:" + dice);
         
-        System.out.println("こんにちは");
-        
-        if (isHungry == 0){
-            System.out.println("お腹いっぱいです");
-        }else{
-            System.out.println("腹ペコです");
-        }
-        
-        if (isHungry == 1){
-            System.out.println(food +"をいただきます");
-        }
-        
-        System.out.println("数当てゲーム");
-        int ans = new java.util.Random().nextInt(10);
-        for (int i = 0; i < 5 ; i++){
-            System.out.println("0~9の数字を入れてください");
-            int num = new java.util.Scanner(System.in).nextInt();
-            if(ans == num){
-                System.out.println("アタリ");
-            }else{
-                System.out.println("違います");
-            }
-        }
-        System.out.println("ゲームを終了します");
     }
 }
--- java4.java
+++ java4.java
@@ -2,12 +2,12 @@ public class java4{
     public static void main (String[] args){
         introduceOneself();
     }
-    public static void introduceOneself(){
+   /* public static void introduceOneself(){
         String name = "hahaha";
         int age = 14;
         double height = 179.4;
         char seiza = '酉';
-        System.out.println("私の名前は" + name + "です。" + "身長は" + height +"年齢は" + age + "干支は" + seiza + "です");
+        System.out.println("私の名前は" + name + "です。" + "身長は" + height +"年齢は" + age + "干支は" + seiza + "です");*/
         
     }
 }
