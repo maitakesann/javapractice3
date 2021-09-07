8f4075c357574e26cf910885c1e9a5ee49e219c4
maitakesann <kankore1204a@yahoo.co.jp>
2021-09-07 08:47:19 +0000

    偶数表記違い

--- java3.java
+++ java3.java
@@ -2,6 +2,12 @@ public class java3{
     public static void main(String[] args){
         int dice = (int)(Math.random()*7) + 1; //1~7までの乱数を発生させる
         System.out.println("サイコろの目:" + dice);
-        
+        if(1 <= dice && dice <= 6){
+            if(dice % 2 == 0){
+                System.out.println("丁です");
+            }else{
+                System.out.println("半です");
+            }
+        }
     }
 }
