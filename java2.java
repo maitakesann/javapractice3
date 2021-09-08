b493f550aaaab63b3f1551489594b10cf54b8289
maitakesann <kankore1204a@yahoo.co.jp>
2021-09-08 04:59:11 +0000

    問題2

--- java2.java
+++ java2.java
@@ -2,8 +2,8 @@ public class java2{
     public static void main(String[] args){
         int dice = new java.util.Random().nextInt(10);
         System.out.println("サイコロの目:"+ dice);
-        if(dice >= 5){
-            System.out.println("5以上です");
+        if(!(dice == 1)){
+            System.out.println("1ではありません");
         }
     }
 }
