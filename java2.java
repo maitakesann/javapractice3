7abea57ca0813ea1036d6dea9ddac521e22ab400
maitakesann <kankore1204a@yahoo.co.jp>
2021-09-08 05:26:25 +0000

    3-16

--- java2.java
+++ java2.java
@@ -2,8 +2,14 @@ public class java2{
     public static void main(String[] args){
         int dice = new java.util.Random().nextInt(100);
         System.out.println("サイコロの目:"+ dice);
-        if(dice <= 10 || dice >= 90){
-            System.out.println("10以下か90以上です");
+        if(dice >= 80){
+            System.out.println("優");
+        }else if(dice <= 80 && dice >= 70){
+            System.out.println("良");
+        }else if(dice <= 70 && dice >=60){
+            System.out.println("ka");
+        }else{
+            System.out.println("huka");
         }
     }
 }
