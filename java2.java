54525fa0559c677b2a91cc0b6eee34038c7e3d05
maitakesann <kankore1204a@yahoo.co.jp>
2021-09-08 05:03:16 +0000

    3-4

--- java2.java
+++ java2.java
@@ -2,8 +2,8 @@ public class java2{
     public static void main(String[] args){
         int dice = new java.util.Random().nextInt(100);
         System.out.println("サイコロの目:"+ dice);
-        if(dice <= 50){
-            System.out.println("50未満です");
+        if(dice <= 10 || dice >= 90){
+            System.out.println("10以下か90以上です");
         }
     }
 }
