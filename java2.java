2efc0e1476d2b251dd627144f57195e1ea3425dc
maitakesann <kankore1204a@yahoo.co.jp>
2021-09-18 13:01:26 +0000

    t3 t5 delete

--- java2.java
+++ java2.java
@@ -15,7 +15,6 @@ public class java2{
         //java5();
         //t3();
         //t4();
-        //t5();
         //t6();
         //t7();
         t8();
@@ -46,57 +45,6 @@ public class java2{
         for(int i = 0; i < y.length; i++){
             y[i] = (int)(Math.random() * 10) + 1;
             System.out.print("a["+ i +"]=" + y[i] + " ");
-        }
-    }
-    
-    public static void t3(){
-        double[] d = {0.2, -5.1, 3.2, 1.8};
-        for(int i = 0; i < d.length; i++){
-            System.out.print("double["+ i +"]=" + d[i] + " ");
-        }
-        System.out.println("");
-        
-    }
-    
-    public static void t4(){
-        int[] date = new int[10];
-        for(int i = 0; i < date.length; i++){
-            date[i] = (int)(Math.random() * 100 ) + 1;
-            System.out.print(date[i] + " ");
-        }
-        System.out.println("");
-        System.out.println("");
-        for(int i = 0; i < date.length; i++){
-            if(date[i] <= 50){
-                System.out.print("50未満：" + date[i] + " ");
-            }
-        }
-        System.out.println("");
-        System.out.println("");
-        for(int i = 0; i < date.length; i++){
-            if(date[i] >= 50){
-                System.out.print("50以上：" + date[i] + " ");
-            }
-        }
-    }
-    
-    public static void t5(){
-        int[] date = new int[10];
-        for(int i = 0; i < date.length; i++){
-            date[i] = new java.util.Random().nextInt(100);
-            System.out.print(date[i] + " ");
-        }
-        System.out.println("");
-        for(int i = 0; i < date.length; i++){
-            if(date[i] % 3 == 0){
-                System.out.print("3の倍数:" + date[i] + " ");
-            }
-        }
-        System.out.println("");
-        for(int i = 0; i < date.length; i++){
-            if(date[i] % 3 != 0){
-                System.out.print("3の倍数以外：" + date[i] + " ");
-            }
         }
     }
     
